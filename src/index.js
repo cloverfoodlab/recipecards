6f49c28715caeb6778b465a1e08938b6c5b31a38
Owen Wang <wangfowen@gmail.com>
2017-05-19 19:29:17 +0800

    Adding in basic build things

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+  <h1>Recipe Cards</h1>,
+  document.getElementById('root')
+);
+
---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loaders: ["style", "css", "sass"]
+    }]
+  }
+};

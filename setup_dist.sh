c6724044b5250b98e76f1fc56147f59df1bfb1bb
Ethan Sherbondy <sherbondye@gmail.com>
2017-05-20 17:36:42 -0400

    try specifying an explicit endpoint since their seems to be a conflict between splat and query params.

--- setup_dist.sh
+++ setup_dist.sh
@@ -3,9 +3,9 @@ cp index.html dist
 
 echo "creating api redirect"
 rm _redirects
-echo '/api/*  https://api.peachworks.com/v1/accounts/'$PEACHWORKS_ACCOUNT_ID'/:splat?access_token='$PEACHWORKS_ACCESS_TOKEN' 200' >> _redirects
-echo '/test/* http://owenwang.com/:splat 200' >> _redirects
-echo '/foo /' >> _redirects
+echo '/api/wtm_recipes page=:page  https://api.peachworks.com/v1/accounts/'$PEACHWORKS_ACCOUNT_ID'/wtm_recipes?access_token='$PEACHWORKS_ACCESS_TOKEN'&page=:page  200' >> _redirects
+echo '/test/*  http://owenwang.com/:splat  200' >> _redirects
+echo '/foo  /' >> _redirects
 
 echo "copying redirects and headers into dist"
 cp _redirects dist

5c3c6d699c02f2a7e1094d7849a13bc36409125f
Owen Wang <wangfowen@gmail.com>
2017-05-20 21:00:07 +0800

    hmm redirects not working as expected

--- setup_dist.sh
+++ setup_dist.sh
@@ -2,7 +2,9 @@ echo "copying index into dist"
 cp index.html dist
 
 echo "creating api redirect"
+rm _redirects
 echo '/api/*  https://api.whentomanage.com/v1/accounts/2985/:splat?access_token='$PEACHWORKS_ACCESS_TOKEN' 200' >> _redirects
 echo '/test/* https://owenwang.com/:splat 200' >> _redirects
+echo '/foo /' >> _redirects
 cp _redirects dist
 

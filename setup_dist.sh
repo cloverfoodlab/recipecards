d7babf3ce10855c26228c71985a0ef55a55f70a4
Owen Wang <wangfowen@gmail.com>
2017-05-20 21:04:37 +0800

    don't need https for test

--- setup_dist.sh
+++ setup_dist.sh
@@ -4,7 +4,7 @@ cp index.html dist
 echo "creating api redirect"
 rm _redirects
 echo '/api/*  https://api.whentomanage.com/v1/accounts/2985/:splat?access_token='$PEACHWORKS_ACCESS_TOKEN' 200' >> _redirects
-echo '/test/* https://owenwang.com/:splat 200' >> _redirects
+echo '/test/* owenwang.com/:splat 200' >> _redirects
 echo '/foo /' >> _redirects
 cp _redirects dist
 

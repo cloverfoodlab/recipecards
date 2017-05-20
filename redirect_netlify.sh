82b2e57b7edf4788414185ae499f4f64262f91c2
Owen Wang <wangfowen@gmail.com>
2017-05-20 20:38:32 +0800

    herp wrong way of doing it, does this work for api access

---
+++ redirect_netlify.sh
@@ -0,0 +1,2 @@
+echo '/api/*  https://api.whentomanage.com/v1/accounts/2985/:splat?accesstoken='$PEACHWORKS_ACCESS_TOKEN' 200' >> dist/_redirects
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 
-/*
-https://api.whentomanage.com/v1/accounts/2985/menu_items?accesstoken=<accesstoken>
-*/
-const accessToken = process.env.PEACHWORKS_ACCESS_TOKEN;
-
 render(
-  <h1>{ accessToken }</h1>,
+  <h1>Recipe Cards</h1>,
   document.getElementById('root')
 );
 

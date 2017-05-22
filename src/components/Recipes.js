234c2cfe684a1f4a05f543c1b84653083f829871
Owen Wang <wangfowen@gmail.com>
2017-05-22 23:54:06 +0800

    axios for making api calls

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 import Recipe from './Recipe'
 
-const Recipes = ({ recipes }) => {
+const Recipes = ({ recipes, onLoad }) => {
   return (
     <div className="recipes">
       <h1>Recipes</h1>
+      <button onClick={onLoad}>Load Recipes</button>
       { recipes.map(recipe => <Recipe { ...recipe } />) }
     </div>
   )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,21 @@
-const defaultState = {
-  recipes: [
-    "recipe1",
-    "recipe2"
-  ]
-}
+const defaultState = [
+  "recipe1",
+  "recipe2"
+]
 
 const recipes = (state = defaultState, action) => {
   switch (action.type) {
+    case 'LOAD_RECIPES':
+      console.log("loading recipes")
+
+      return state
+    case 'LOAD_RECIPES_SUCCESS':
+      console.log(payload)
+
+      return [...state,
+        "recipe3"
+        //include payload.???
+      ]
     default:
       return state
   }

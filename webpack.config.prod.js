4cf0e51ffa0e08f8afc06069423da0580da306ae
Ethan Sherbondy <sherbondye@gmail.com>
2017-05-25 14:05:43 -0400

    had to move some deps out of dev for production now build to work.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loader: 'babel',
+      loader: 'babel-loader',
       include: path.join(__dirname, 'src'),
       exclude: /node_modules/
     }, {
